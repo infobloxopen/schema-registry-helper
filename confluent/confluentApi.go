e04b18167b97c6584eaa261f1f41a4cf7fa5440d
Broc Woodworth <bwoodworth@infoblox.com>
2020-09-26 10:55:45 -0700

    refactoring and bug fixes (#3)
    * add cli tool
    
    * Fixing some bugs and a bit of refactoring
    
    * go fmt

--- confluent/confluentApi.go
+++ confluent/confluentApi.go
@@ -57,7 +57,7 @@ type credentials struct {
 
 type schemaRequest struct {
 	Schema     string      `json:"schema"`
-	SchemaType string  `json:"schemaType"`
+	SchemaType string      `json:"schemaType"`
 	References []Reference `json:"references"`
 }
 
@@ -85,7 +85,7 @@ const (
 	contentType                 = "application/vnd.schemaregistry.v1+json"
 )
 
-var ErrNotFound = "404 Not Found: Schema not found"
+var ErrNotFound = "404 Not Found"
 
 // CreateSchemaRegistryClient creates a client that allows
 // interactions with Schema Registry over HTTP. Applications
--- schemaRegistryCliTool.go
+++ schemaRegistryCliTool.go
@@ -1,4 +1,4 @@
-package schema_registry_helper
+package main
 
 import (
 	"flag"
@@ -11,7 +11,7 @@ import (
 	"github.com/infobloxopen/schema-registry-helper/confluent"
 )
 
-func validateFlags(schemaRegistryUrlPtr, schemaTypePtr, schemaDirectoryPtr *string) (string, confluent.SchemaType, string){
+func validateFlags(schemaRegistryUrlPtr, schemaTypePtr, schemaDirectoryPtr *string) (string, confluent.SchemaType, string) {
 	if *schemaRegistryUrlPtr == "" || *schemaDirectoryPtr == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
@@ -31,7 +31,7 @@ func validateFlags(schemaRegistryUrlPtr, schemaTypePtr, schemaDirectoryPtr *stri
 	return *schemaRegistryUrlPtr, schemaType, *schemaDirectoryPtr
 }
 
-func parseNamespaces(schemaDirectory string) ([]string){
+func parseNamespaces(schemaDirectory string) []string {
 	files, err := ioutil.ReadDir(schemaDirectory)
 	if err != nil {
 		fmt.Printf("Error reading input directory: %v", err)
@@ -48,55 +48,72 @@ func parseNamespaces(schemaDirectory string) ([]string){
 		fmt.Printf("Schema directory contains no subdirectories. No schemas to update.\r\n")
 		os.Exit(1)
 	}
+	return namespaces
 }
 
 func main() {
 	schemaRegistryUrlPtr := flag.String("url", "", "URL of the Schema Registry (Required)")
 	schemaTypePtr := flag.String("type", "", "Schema Type {json|protobuf|avro} (Required)")
-	schemaDirectoryPtr := flag.String("directory", "", "Directory containing the schema files (Required)")
-	singleSchemaPtr := flag.String("single", "", "Export only the chosen schema file. " +
-									"If left blank, all schemas in the directory will be exported (Optional)")
+	inputSchemaPtr := flag.String("inputschema", "", "Either the directory containing the schema files, or a specific schema file. "+
+		"If a directory is given, tool will automatically import all schema files within subdirectories.")
 	flag.Parse()
 
-	schemaRegistry, schemaType, schemaDirectory := validateFlags(schemaRegistryUrlPtr, schemaTypePtr, schemaDirectoryPtr)
+	schemaRegistry, schemaType, inputSchema := validateFlags(schemaRegistryUrlPtr, schemaTypePtr, inputSchemaPtr)
 	schemaRegistryClient := confluent.CreateSchemaRegistryClient(schemaRegistry)
-	namespaces := parseNamespaces(schemaDirectory)
-
-	if *singleSchemaPtr == "" {
+	fi, err := os.Stat(inputSchema)
+	if err != nil {
+		fmt.Printf("Error reading the input schema: %v\r\n", err)
+		os.Exit(1)
+	}
+	if fi.Mode().IsDir() {
+		fmt.Printf("Reading input as a directory. Automatically exporting all schemas in this directory tree: <%v>\r\n", inputSchema)
+		namespaces := parseNamespaces(inputSchema)
 		for _, n := range namespaces {
-			namespaceDirectory := schemaDirectory + "/" + n
-			fmt.Printf("Exporting schemas in directory %v", namespaceDirectory)
+			namespaceDirectory := inputSchema + "/" + n
+			fmt.Printf("Exporting schemas in directory %v...\r\n", namespaceDirectory)
 			files, err := ioutil.ReadDir(namespaceDirectory)
 			if err != nil {
-				fmt.Printf("Error reading input directory %v, skipping...", namespaceDirectory)
+				fmt.Printf("Error reading input directory %v, skipping...\r\n", namespaceDirectory)
 				break
 			}
 			for _, f := range files {
-				exportSchema(f.Name(), namespaceDirectory, schemaType, *schemaRegistryClient)
+				filePath := namespaceDirectory + "/" + f.Name()
+				topic := n + "-" + strings.TrimSuffix(f.Name(), filepath.Ext(f.Name()))
+				exportSchema(filePath, topic, schemaType, *schemaRegistryClient)
 			}
 		}
 	} else {
-		fmt.Printf("Feature under construction")
+		fmt.Printf("Reading input as a single file. Exporting just this file: <%v>\r\n", inputSchema)
+		filePathParts := strings.Split(inputSchema, "/")
+		if len(filePathParts) < 2 {
+			fmt.Printf("Please include the file's parent directory as part of the input argument.\r\n")
+			os.Exit(1)
+		}
+		namespace := filePathParts[len(filePathParts)-2]
+		fileName := filePathParts[len(filePathParts)-1]
+		filePrefix := strings.TrimSuffix(fileName, filepath.Ext(fileName))
+		exportSchema(inputSchema, namespace+"-"+filePrefix, schemaType, *schemaRegistryClient)
 	}
 }
 
-func exportSchema(fileName, namespaceDirectory string, schemaType confluent.SchemaType, src confluent.SchemaRegistryClient) {
-	filePath := namespaceDirectory + "/" + fileName
-	topic := n + "-" + strings.TrimSuffix(fileName, filepath.Ext(fileName))
+func exportSchema(filePath, topic string, schemaType confluent.SchemaType, src confluent.SchemaRegistryClient) {
 	schemaBytes, _ := ioutil.ReadFile(filePath)
-	_, err := src.CheckSchema(topic, string(schemaBytes), schemaType, false)
-	if err.Error() != confluent.ErrNotFound {
-		panic(fmt.Sprintf("Error checking the schema %v: %s", filePath, err))
-	} else {
-		_, err := src.CreateSchema(topic, string(schemaBytes), schemaType, false)
+	schema, err := src.CheckSchema(topic, string(schemaBytes), schemaType, false)
+	if err != nil && !strings.Contains(err.Error(), confluent.ErrNotFound) {
+		panic(fmt.Sprintf("  Error checking the schema %v: %s", filePath, err))
+	} else if err != nil {
+		schema, err := src.CreateSchema(topic, string(schemaBytes), schemaType, false)
 		if err != nil {
-			panic(fmt.Sprintf("Error creating the schema: %v: %s", filePath, err))
+			panic(fmt.Sprintf("  Error creating the schema: %v: %s", filePath, err))
 		}
+		fmt.Printf("  New schema successfully created for topic %v - Schema version %v.\r\n", topic, schema.Version())
+	} else {
+		fmt.Printf("  Schema already exists for topic %v - Schema version %v.\r\n", topic, schema.Version)
 	}
 }
+
 //TODO (not in this tool):
 // for each .proto file in input argument:
 // grab namespace from go_package using bash script; e.g. "pb"
 // create schema/pb directory
 // protoc --jsonschema_out=schema/pb blahblah
-
